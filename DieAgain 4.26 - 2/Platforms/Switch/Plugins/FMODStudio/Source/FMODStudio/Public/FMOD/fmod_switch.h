1de9b4ef60e46642ee578422873c399f71d69910
Adria <adriaortiznavarro@enti.cat>
2021-06-21 14:19:35 +0200

    FMOD is no more (Now sound is with UE4) + Cutscene Level10 is at gold

--- DieAgain 4.26 - 2/Platforms/Switch/Plugins/FMODStudio/Source/FMODStudio/Public/FMOD/fmod_switch.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "fmod.h"
-
-extern "C" FMOD_RESULT F_API FMOD_Switch_SetHTCSEnabled(FMOD_BOOL enabled);
--- DieAgain 4.26 - 2/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.16.27039
-#include "E:/DieAgainProjects/DieAgain GitHub/DieAgain/DieAgain 4.26 - 2/Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Development/FMODStudioEditor/PCH.FMODStudioEditor.h"
