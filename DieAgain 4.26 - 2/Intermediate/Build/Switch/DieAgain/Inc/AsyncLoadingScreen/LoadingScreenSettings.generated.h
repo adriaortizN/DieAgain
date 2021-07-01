c18dcf0b93d7beadee440153b4647a0ea8d70495
Adria <adriaortiznavarro@enti.cat>
2021-07-01 18:39:20 +0200

    More Switch Build files

---
+++ DieAgain 4.26 - 2/Intermediate/Build/Switch/DieAgain/Inc/AsyncLoadingScreen/LoadingScreenSettings.generated.h
@@ -0,0 +1,227 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ASYNCLOADINGSCREEN_LoadingScreenSettings_generated_h
+#error "LoadingScreenSettings.generated.h already included, missing '#pragma once' in LoadingScreenSettings.h"
+#endif
+#define ASYNCLOADINGSCREEN_LoadingScreenSettings_generated_h
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_620_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FDualSidebarLayoutSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FDualSidebarLayoutSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_573_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FSidebarLayoutSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FSidebarLayoutSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_530_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FLetterboxLayoutSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FLetterboxLayoutSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_499_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FCenterLayoutSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FCenterLayoutSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_464_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FClassicLayoutSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FClassicLayoutSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_364_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FALoadingScreenSettings_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FALoadingScreenSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_327_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FLoadingCompleteTextSettings_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FLoadingCompleteTextSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_299_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FTipSettings_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FTipSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_227_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FLoadingWidgetSettings_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FLoadingWidgetSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_195_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FBackgroundSettings_Statics; \
+	static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FBackgroundSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_168_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FImageSequenceSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FImageSequenceSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_146_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FCircularThrobberSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FCircularThrobberSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_120_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FThrobberSettings_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FThrobberSettings>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_94_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FTextAppearance_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FTextAppearance>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_80_GENERATED_BODY \
+	friend struct Z_Construct_UScriptStruct_FWidgetAlignment_Statics; \
+	ASYNCLOADINGSCREEN_API static class UScriptStruct* StaticStruct();
+
+
+template<> ASYNCLOADINGSCREEN_API UScriptStruct* StaticStruct<struct FWidgetAlignment>();
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_SPARSE_DATA
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_RPC_WRAPPERS
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_RPC_WRAPPERS_NO_PURE_DECLS
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesULoadingScreenSettings(); \
+	friend struct Z_Construct_UClass_ULoadingScreenSettings_Statics; \
+public: \
+	DECLARE_CLASS(ULoadingScreenSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/AsyncLoadingScreen"), NO_API) \
+	DECLARE_SERIALIZER(ULoadingScreenSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_INCLASS \
+private: \
+	static void StaticRegisterNativesULoadingScreenSettings(); \
+	friend struct Z_Construct_UClass_ULoadingScreenSettings_Statics; \
+public: \
+	DECLARE_CLASS(ULoadingScreenSettings, UDeveloperSettings, COMPILED_IN_FLAGS(0 | CLASS_DefaultConfig | CLASS_Config), CASTCLASS_None, TEXT("/Script/AsyncLoadingScreen"), NO_API) \
+	DECLARE_SERIALIZER(ULoadingScreenSettings) \
+	static const TCHAR* StaticConfigName() {return TEXT("Game");} \
+
+
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ULoadingScreenSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ULoadingScreenSettings) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ULoadingScreenSettings); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ULoadingScreenSettings); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ULoadingScreenSettings(ULoadingScreenSettings&&); \
+	NO_API ULoadingScreenSettings(const ULoadingScreenSettings&); \
+public:
+
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ULoadingScreenSettings(ULoadingScreenSettings&&); \
+	NO_API ULoadingScreenSettings(const ULoadingScreenSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ULoadingScreenSettings); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ULoadingScreenSettings); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ULoadingScreenSettings)
+
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_PRIVATE_PROPERTY_OFFSET
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_662_PROLOG
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_PRIVATE_PROPERTY_OFFSET \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_SPARSE_DATA \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_RPC_WRAPPERS \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_INCLASS \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_PRIVATE_PROPERTY_OFFSET \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_SPARSE_DATA \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_RPC_WRAPPERS_NO_PURE_DECLS \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_INCLASS_NO_PURE_DECLS \
+	DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h_665_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ASYNCLOADINGSCREEN_API UClass* StaticClass<class ULoadingScreenSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID DieAgain_4_26___2_Plugins_AsyncLoadingScreen_4_26_Source_AsyncLoadingScreen_Public_LoadingScreenSettings_h
+
+
+#define FOREACH_ENUM_ELOADINGWIDGETTYPE(op) \
+	op(ELoadingWidgetType::LWT_Horizontal) \
+	op(ELoadingWidgetType::LWT_Vertical) 
+
+enum class ELoadingWidgetType : uint8;
+template<> ASYNCLOADINGSCREEN_API UEnum* StaticEnum<ELoadingWidgetType>();
+
+#define FOREACH_ENUM_ELOADINGICONTYPE(op) \
+	op(ELoadingIconType::LIT_Throbber) \
+	op(ELoadingIconType::LIT_CircularThrobber) \
+	op(ELoadingIconType::LIT_ImageSequence) 
+
+enum class ELoadingIconType : uint8;
+template<> ASYNCLOADINGSCREEN_API UEnum* StaticEnum<ELoadingIconType>();
+
+#define FOREACH_ENUM_EASYNCLOADINGSCREENLAYOUT(op) \
+	op(EAsyncLoadingScreenLayout::ALSL_Classic) \
+	op(EAsyncLoadingScreenLayout::ALSL_Center) \
+	op(EAsyncLoadingScreenLayout::ALSL_Letterbox) \
+	op(EAsyncLoadingScreenLayout::ALSL_Sidebar) \
+	op(EAsyncLoadingScreenLayout::ALSL_DualSidebar) 
+
+enum class EAsyncLoadingScreenLayout : uint8;
+template<> ASYNCLOADINGSCREEN_API UEnum* StaticEnum<EAsyncLoadingScreenLayout>();
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
